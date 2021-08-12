87a7e374450034a12a0ef20a0f7adad4f8af69fe
natasharrison <natasharrison@gmail.com>
2021-08-12 08:47:51 -0700

    logo images-nav bar-client auth

--- client/src/component/Nav/index.js
+++ client/src/component/Nav/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import '../Nav/style.css';
+import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 function Nav() {
 
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    };
+
     return (
         <header>
+            <div className="container flex-row justify-space-between-lg justify-center align-center">
+                <Link to="/">
+                    <img src={require('../../assets/img/banner-logo.jpg').default} alt="Health Wealth"/>
+                </Link>
 
-
+                <nav className="text-center">
+                    {Auth.loggedIn() ? (
+                        <>
+                            <Link to="/profile"> My Profile </Link>
+                            <a href="/" onClick={logout}>
+                                Logout
+                            </a>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/login">Login</Link>
+                            <Link to="/signup">Signup</Link>
+                        </>
+                    )}
+                </nav>
+            </div>
         </header>
     )
 }
