15f0a2642b61f0f28fe2f78f277fdaddc02d66c5
samuel Odubamowo <wemsamuel@gmail.com>
2021-08-12 01:09:35 -0700

    updated server file with apollo graphQl instead of local graphQl

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 const express = require('express');
-const {ApolloServer} = require('apollo-server-express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
-const {typeDefs, resolvers} = require('./schemas');
-const { authMiddleware } = require('./utils/auth')
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+const { authMiddleware } = require('./utils/auth');
 
-const app = express();
 const PORT = process.env.PORT || 3001;
+const app = express();
 const server = new ApolloServer({ 
   typeDefs, 
   resolvers, 
@@ -30,9 +30,9 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
