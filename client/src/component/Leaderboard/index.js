8fe04fc1bef3b8fed705556260917640d250ea57
natasharrison <natasharrison@gmail.com>
2021-08-13 08:27:46 -0700

    initial leaderboard files

---
+++ client/src/component/Leaderboard/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../Leaderboard/style.css';
+
+function Leaderboard() {
+
+    return (
+        <div>
+
+
+        </div>
+    )
+
+}
+
+export default Leaderboard;
