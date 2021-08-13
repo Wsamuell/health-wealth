eb1588f749ee39aefcf263a65dfa9a076c184801
Cade Ellsworth <caedmd2100@gmail.com>
2021-08-12 18:50:02 -0700

    creates Profile.js file, creates Recipes and Regiment components, comments on future functionality

---
+++ client/src/component/Recipes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Recipes () {
+    return (
+        <div>
+            {/* Here, we would make a query to get all of the user's recipes from the database.
+            Then all the recipes would be formatted as cards, maybe using forEach. */}
+        </div>
+    )
+}
+
+export default Recipes;
---
+++ client/src/component/Regiment/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Regiment () {
+    return (
+        <div>
+            {/* Here, we would make a query to get all of the user's goals from the database.
+            Then all the goals would be formatted as cards, maybe using forEach. Depending on 
+            how we want to do it, we could even have a Goal component to help with this. */}
+        </div>
+    )
+}
+
+export default Regiment;
