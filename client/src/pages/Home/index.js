f219165cb2829de4c8e48291bc0f67007124b0a5
samuel Odubamowo <wemsamuel@gmail.com>
2021-08-14 16:16:08 -0700

    working on home page set up, this is jjust the imports

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import { useQuery } from '@apollo/client';
+import { QUERY_ME } from '../../utils/queries';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
+import FriendsList from '../../component/FriendList';
+import { FormControl, InputGroup, Button } from 'react-bootstrap'
 import './style.css';
 
 function Home() {
 
     return (
         <div className='home'>
-            
+            <InputGroup className="mb-3 w-50 align-middle">
+                <FormControl
+                    placeholder="Search for other users"
+                    aria-label="Recipient's username"
+                    aria-describedby="basic-addon2"
+                />
+                <Button variant="outline-secondary" id="button-addon2">
+                    Search
+                </Button>
+            </InputGroup>
+
         </div>
     )
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 
-export const GET_ME = gql`
+export const QUERY_ME = gql`
 query {
     me {
       username
