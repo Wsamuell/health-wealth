45c9b54937cec8ec88b14bb387b4d838539acda3
natasharrison <natasharrison@gmail.com>
2021-08-14 20:53:08 -0700

    initial shop html

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -1,11 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from "react-router-dom";
+// modeled after ShopShop components/ProductItem
+
+// purchase function that will remove points from your account 
+
+// where do we define 'name, image, price'? How are those all related to one another?
 
 function Shop() {
 
     return (
-        <div>
-
-        </div>
+    <div className="card px-1 py-1">
+      <Link to={`/products/${_id}`}>
+        <img
+          alt={name}
+          src={`img/user_icons/${svg}`}
+        />
+        <p>{name}</p>
+      </Link>
+      <div>
+        <span>${price}</span>
+      </div>
+      <button onClick={purchase}>Purchase</button>
+    </div>
     )
 }
 
