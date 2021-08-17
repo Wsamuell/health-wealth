9f6dab227cd500dcff3c74f72394ab6c0c8039df
natasharrison <natasharrison@gmail.com>
2021-08-17 09:24:10 -0700

    service-worker

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -9,13 +9,13 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./client/src/App.js":
+/***/ "./client/src/app.js":
 /*!***************************!*\
-  !*** ./client/src/App.js ***!
+  !*** ./client/src/app.js ***!
   \***************************/
 /***/ (() => {
 
-eval("throw new Error(\"Module parse failed: Unexpected token (44:4)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n| function App() {\\n|   return (\\n>     <ApolloProvider client={client}>\\n|       <Router>\\n|         <div className=\\\"\\\">\");\n\n//# sourceURL=webpack://project-three/./client/src/App.js?");
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\client\\\\src\\\\app.js: Support for the experimental syntax 'jsx' isn't currently enabled (44:5):\\n\\n\\u001b[0m \\u001b[90m 42 |\\u001b[39m \\u001b[36mfunction\\u001b[39m \\u001b[33mApp\\u001b[39m() {\\u001b[0m\\n\\u001b[0m \\u001b[90m 43 |\\u001b[39m   \\u001b[36mreturn\\u001b[39m (\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 44 |\\u001b[39m     \\u001b[33m<\\u001b[39m\\u001b[33mApolloProvider\\u001b[39m client\\u001b[33m=\\u001b[39m{client}\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m    |\\u001b[39m     \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 45 |\\u001b[39m       \\u001b[33m<\\u001b[39m\\u001b[33mRouter\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 46 |\\u001b[39m         \\u001b[33m<\\u001b[39m\\u001b[33mdiv\\u001b[39m className\\u001b[33m=\\u001b[39m\\u001b[32m\\\"\\\"\\u001b[39m\\u001b[33m>\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 47 |\\u001b[39m           {loggedIn \\u001b[33m&&\\u001b[39m (\\u001b[0m\\n\\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\\n    at Parser._raise (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:798:17)\\n    at Parser.raiseWithData (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:791:17)\\n    at Parser.expectOnePlugin (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:3272:18)\\n    at Parser.parseExprAtom (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11514:20)\\n    at Parser.parseExprSubscripts (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11081:23)\\n    at Parser.parseUpdate (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11061:21)\\n    at Parser.parseMaybeUnary (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:11039:23)\\n    at Parser.parseExprOps (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10882:23)\\n    at Parser.parseMaybeConditional (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10856:23)\\n    at Parser.parseMaybeAssign (C:\\\\Users\\\\natas\\\\Desktop\\\\repo\\\\health-wealth\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:10814:21)\");\n\n//# sourceURL=webpack://project-three/./client/src/app.js?");
 
 /***/ })
 
@@ -26,7 +26,7 @@ eval("throw new Error(\"Module parse failed: Unexpected token (44:4)\\nYou may n
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
 /******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./client/src/App.js"]();
+/******/ 	__webpack_modules__["./client/src/app.js"]();
 /******/ 	
 /******/ })()
 ;
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,34 @@
 const path = require('path');
 const webpack = require("webpack");
+const babel = require("@babel/core");
 
 module.exports = {
-  entry: './client/src/App.js',
+  entry: './client/src/app.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
   },
-  plugins:[
-  new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  }),
-],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          cacheDirectory: true,
+          presets: ['react']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
+  ],
   mode: 'development'
 };
