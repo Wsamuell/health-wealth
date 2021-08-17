10b6816ed6f0b7bf4d20244434b2d176d460da81
natasharrison <natasharrison@gmail.com>
2021-08-17 15:23:52 -0700

    badge box

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -1,72 +1,69 @@
-import React from 'react';
-import { ADD_POINTS } from '../../utils/mutations';
-import { useMutation } from '@apollo/client';
-import './style.css';
+import React from "react";
+import { ADD_POINTS } from "../../utils/mutations";
+import { useMutation } from "@apollo/client";
+import { Card } from "react-bootstrap";
+import "./style.css";
 
 function Shop(item) {
   const [purchase] = useMutation(ADD_POINTS);
 
   const badge = [
-
     {
-      "icon": "bb8",
-      "price": -20,
-      "svgFile": 'bb8.svg'
+      icon: "bb8",
+      price: -20,
+      svgFile: "bb8.svg",
     },
     {
-      "icon": "Bronze",
-      "price": -50,
-      "svgFile": 'bronzemedalv1.svg'
+      icon: "Bronze",
+      price: -50,
+      svgFile: "bronzemedalv1.svg",
     },
     {
-      "icon": "Silver",
-      "price": -100,
-      "svgFile": 'silvericon.svg'
+      icon: "Silver",
+      price: -100,
+      svgFile: "silvericon.svg",
     },
     {
-      "icon": "Gold",
-      "price": -150,
-      "svgFile": 'goldicon.svg'
+      icon: "Gold",
+      price: -150,
+      svgFile: "goldicon.svg",
     },
     {
-      "icon": "Heart",
-      "price": -20,
-      "svgFile": 'hearticon.svg'
+      icon: "Heart",
+      price: -20,
+      svgFile: "hearticon.svg",
     },
     {
-      "icon": "Nucleus",
-      "price": -20,
-      "svgFile": 'nucleusicon.svg'
+      icon: "Nucleus",
+      price: -20,
+      svgFile: "nucleusicon.svg",
     },
     {
-      "icon": "Pizza",
-      "price": -20,
-      "svgFile": 'pizzaicon.svg'
+      icon: "Pizza",
+      price: -20,
+      svgFile: "pizzaicon.svg",
     },
     {
-      "icon": "Pirate Skull",
-      "price": -40,
-      "svgFile": 'skullpirateicon2.svg'
-    }
-  ]
+      icon: "Pirate Skull",
+      price: -40,
+      svgFile: "skullpirateicon2.svg",
+    },
+  ];
 
   return (
     <div className="shop-page">
-      <h2 className="shop-title">
-        Shop
-      </h2>
-      <div className="card px-1 py-1" id="badgeCard">
-        <img
-          // alt= ${badge.icon}
-          src={`img/user_icons/${badge.svgFile}`}
-        />
+      <h2 className="shop-title">Shop coming soon!</h2>
+      <Card style={{ width: "18rem " }}>
+        {/* <Card.Img variant="top" src={require(`../../assets/user_icons/${badge.svgFile}`).default} alt={badge.icon}/> */}
         <div>
-          <span>${badge.price}</span>
+          <Card.Text>PRICE{badge.price}</Card.Text>
         </div>
-        <button className='btn btn-primary ml-auto' onClick={purchase}>Purchase</button>
-      </div>
+        <button className="btn btn-primary ml-auto" onClick={purchase}>
+          Purchase
+        </button>
+      </Card>
     </div>
-  )
+  );
 }
 
 export default Shop;
