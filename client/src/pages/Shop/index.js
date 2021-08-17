927c870e767ca71603f0682bc91cd22089c6c6ef
natasharrison <natasharrison@gmail.com>
2021-08-17 14:29:21 -0700

    shop array and styling

--- client/src/pages/Shop/index.js
+++ client/src/pages/Shop/index.js
@@ -1,31 +1,72 @@
 import React from 'react';
-import { Link } from "react-router-dom";
-// modeled after ShopShop components/ProductItem
-
-// purchase function that will remove points from your account 
-
-// where do we define 'name, image, price'? How are those all related to one another?
+import { ADD_POINTS } from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
+import './style.css';
 
 function Shop(item) {
+  const [purchase] = useMutation(ADD_POINTS);
 
-    return (
-    <div className="card px-1 py-1">
-      {/* <Link to={`/products/${_id}`}>
+  const badge = [
+
+    {
+      "icon": "bb8",
+      "price": -20,
+      "svgFile": 'bb8.svg'
+    },
+    {
+      "icon": "Bronze",
+      "price": -50,
+      "svgFile": 'bronzemedalv1.svg'
+    },
+    {
+      "icon": "Silver",
+      "price": -100,
+      "svgFile": 'silvericon.svg'
+    },
+    {
+      "icon": "Gold",
+      "price": -150,
+      "svgFile": 'goldicon.svg'
+    },
+    {
+      "icon": "Heart",
+      "price": -20,
+      "svgFile": 'hearticon.svg'
+    },
+    {
+      "icon": "Nucleus",
+      "price": -20,
+      "svgFile": 'nucleusicon.svg'
+    },
+    {
+      "icon": "Pizza",
+      "price": -20,
+      "svgFile": 'pizzaicon.svg'
+    },
+    {
+      "icon": "Pirate Skull",
+      "price": -40,
+      "svgFile": 'skullpirateicon2.svg'
+    }
+  ]
+
+  return (
+    <div className="shop-page">
+      <h2 className="shop-title">
+        Shop
+      </h2>
+      <div className="card px-1 py-1" id="badgeCard">
         <img
-          alt={name}
-          src={`img/user_icons/${svg}`}
+          // alt= ${badge.icon}
+          src={`img/user_icons/${badge.svgFile}`}
         />
-        <p>{name}</p>
-      </Link>
-      <div>
-        <span>${price}</span>
+        <div>
+          <span>${badge.price}</span>
+        </div>
+        <button className='btn btn-primary ml-auto' onClick={purchase}>Purchase</button>
       </div>
-      <button onClick={purchase}>Purchase</button> */}
-      <p>image of SVG</p>
-      <p>price of SVG</p>
-      <button>Purchase</button>
     </div>
-    )
+  )
 }
 
 export default Shop;
